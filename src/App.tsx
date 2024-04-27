c02930568f162d3882dc6f320e598a9b3fbbb0ea
Tucker Fritz <tuckerpfritz@gmail.com>
2024-04-27 18:59:56 -0500

    Implement code splitting.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
+import { Suspense, lazy } from "react";
 import { Route, Switch } from "wouter";
-import Demo from "./games/demo/Demo";
-import Home from "./Home";
-import Tutorial from "./games/tutorial/Tutorial";
+import { LoadingSpinner } from "@/components/ui/spinner";
+const Demo = lazy(() => import("./games/demo/Demo"));
+const Home = lazy(() => import("./Home"));
+const Tutorial = lazy(() => import("./games/tutorial/Tutorial"));
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/phaser-demo" component={Demo} />
-      <Route path="/phaser-tutorial" component={Tutorial} />
-      <Route>404</Route>
-    </Switch>
+    <Suspense
+      fallback={
+        <div className="grid h-screen w-screen place-items-center">
+          <LoadingSpinner size={75} />
+        </div>
+      }
+    >
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/phaser-demo" component={Demo} />
+        <Route path="/phaser-tutorial" component={Tutorial} />
+        <Route>404</Route>
+      </Switch>
+    </Suspense>
   );
 }
 
