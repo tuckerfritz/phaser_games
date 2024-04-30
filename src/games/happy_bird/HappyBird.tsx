a8b9341b7e055d8dc26bf42dff00f206932c7576
Tucker Fritz <tuckerpfritz@gmail.com>
2024-04-29 22:00:10 -0500

    Complete implementation of 'Happy Bird'.

--- src/games/happy_bird/HappyBird.tsx
+++ src/games/happy_bird/HappyBird.tsx
@@ -1,9 +1,13 @@
 import { Container, Overlay, Root } from "@/components/ui/layout";
-import { useLayoutEffect, useRef } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { Game } from "phaser";
 import StartGame from "./main";
+import { EventBus } from "../EventBus";
+import { Button } from "@/components/ui/button";
 
 const HappyBird = () => {
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [score, setScore] = useState(0);
   const gameRef = useRef<Game>();
 
   useLayoutEffect(() => {
@@ -19,10 +23,42 @@ const HappyBird = () => {
     };
   }, []);
 
+  useEffect(() => {
+    EventBus.on("game-over", () => {
+      setIsGameOver(true);
+    });
+
+    EventBus.on("update-score", (score: number) => {
+      setScore(score);
+    });
+
+    return () => {
+      EventBus.removeAllListeners();
+    };
+  }, []);
+
+  const startOver = () => {
+    setIsGameOver(false);
+    setScore(0);
+    if (gameRef) {
+      gameRef.current.destroy(true);
+      gameRef.current = StartGame("game-container");
+    }
+  };
+
   return (
     <Root>
       <Container id="game-container" />
-      <Overlay className="grid place-items-center"></Overlay>
+      <Overlay className="grid">
+        <div className="text-5xl font-semibold text-white justify-self-start ml-2  ">
+          {score}
+        </div>
+        {isGameOver && (
+          <Button className="justify-self-center" onClick={startOver}>
+            Start Over
+          </Button>
+        )}
+      </Overlay>
     </Root>
   );
 };
--- src/games/happy_bird/scenes/Game.ts
+++ src/games/happy_bird/scenes/Game.ts
@@ -7,8 +7,8 @@ export class Game extends Scene {
   private pipes: Phaser.Physics.Arcade.StaticGroup;
   private spacebar: Phaser.Input.Keyboard.Key;
   private score: number = 0;
-  private gameOver: boolean = false;
   private flapped: boolean = false;
+  private distanceMoved: number = 0;
 
   constructor() {
     super("Game");
@@ -21,12 +21,14 @@ export class Game extends Scene {
       frameHeight: 16,
     });
     this.load.image("middle-pipe", "assets/happy_bird/middle-pipe.png");
-    this.load.image("middle-pipe", "assets/happy_bird/end-pipe.png");
+    this.load.image("end-pipe", "assets/happy_bird/end-pipe.png");
   }
 
   create() {
+    // Set the background for the game
     this.bg = this.add.tileSprite(0, 0, 512, 512, "background").setScale(2, 2);
 
+    // Create the player sprite, collision, and animations
     this.player = this.physics.add.sprite(256, 256, "bird").setScale(2);
     this.player.setBounce(0.2);
     this.player.setCollideWorldBounds(true);
@@ -34,10 +36,7 @@ export class Game extends Scene {
     this.player.body.world.on("worldbounds", (...args) => {
       const down: boolean = args[2];
       if (down) {
-        this.player.setTint(0xff0000);
-        this.game.pause();
-        this.gameOver = true;
-        EventBus.emit("game-over");
+        this.processGameOver();
       }
     });
     this.anims.create({
@@ -47,13 +46,53 @@ export class Game extends Scene {
       repeat: 0,
     });
 
+    // Construct pipe container out of pipe sprites
+    this.pipes = this.physics.add.staticGroup();
+    this.createPipes();
+
+    // Set collision between the player and the pipes
+    this.physics.add.collider(this.player, this.pipes);
+    this.physics.add.overlap(
+      this.player,
+      this.pipes,
+      this.processGameOver,
+      null,
+      this,
+    );
+
+    // Create spacebar input
     this.spacebar = this.input.keyboard.addKey(
       Phaser.Input.Keyboard.KeyCodes.SPACE,
     );
   }
 
   update() {
+    // Scroll the background
     this.bg.setTilePosition(this.bg.tilePositionX + 0.1);
+
+    // Scroll the pipes & destroy the ones that are off screen to the left
+    this.pipes.incX(-1);
+    this.pipes.children
+      .getArray()
+      .forEach((pipe: Phaser.Physics.Arcade.Sprite) => {
+        if (pipe !== undefined) {
+          pipe.refreshBody();
+          if (pipe.x <= -16) {
+            this.pipes.remove(pipe, true, true);
+          }
+        }
+        return true;
+      });
+
+    // Keep track of how many pixels the player has moved
+    this.distanceMoved += 1;
+    if (this.distanceMoved % 256 === 0) {
+      this.score += 1;
+      EventBus.emit("update-score", this.score);
+      this.createPipes(); // Create another obstacle very 50 pixels
+    }
+
+    // Process player inputs
     if (this.spacebar.isDown && this.flapped === false) {
       this.player.setVelocityY(-250);
       this.flapped = true;
@@ -61,5 +100,26 @@ export class Game extends Scene {
       this.player.anims.play("space");
       this.flapped = false;
     }
+  }
+
+  createPipes() {
+    const bottomPipeHeight = Phaser.Math.Between(2, 25);
+    const topPipeHeight = 25 - bottomPipeHeight + 2;
+    for (let i = 0; i < bottomPipeHeight; ++i) {
+      i === bottomPipeHeight - 1
+        ? this.pipes.create(528, i * 16, "end-pipe")
+        : this.pipes.create(528, i * 16, "middle-pipe");
+    }
+    for (let i = 0; i < topPipeHeight; ++i) {
+      i === topPipeHeight - 1
+        ? this.pipes.create(528, 512 - i * 16, "end-pipe")
+        : this.pipes.create(528, 512 - i * 16, "middle-pipe");
+    }
+  }
+
+  processGameOver() {
+    this.player.setTint(0xff0000);
+    this.game.pause();
+    EventBus.emit("game-over");
   }
 }
